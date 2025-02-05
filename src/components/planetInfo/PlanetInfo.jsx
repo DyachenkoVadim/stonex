0601d8f8b0e6bd6b421496a4fef3d838a5be8f21
Vadim <vadimdyachenko000@gmail.com>
2025-02-05 13:43:17 +0200

    fetchin data from server

--- src/components/planetInfo/PlanetInfo.jsx
+++ src/components/planetInfo/PlanetInfo.jsx
@@ -1,8 +1,49 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./planetInfo.css";
 import { data } from "../../utils/data";
+import axios from "axios";
 
 function PlanetInfo({ planetName }) {
+  const PRODUCTS_API_URL = import.meta.env.VITE_PRODUCTS_API;
+  const CATEGORIES_API_URL = import.meta.env.VITE_CATEGORIES_API;
+  const [products, setProducts] = useState([]);
+  const [categoryID, setCategoryID] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        // Отримуємо список категорій
+        const categoriesResponse = await axios.get(CATEGORIES_API_URL);
+
+        // Шукаємо категорію за назвою планети
+        const planetCategory = categoriesResponse.data.find(
+          (category) => category.name.toLowerCase() === planetName.toLowerCase()
+        );
+
+        // Якщо знайшли категорію, то зберігаємо її _id
+        if (planetCategory) {
+          setCategoryID(planetCategory._id);
+        }
+
+        // Отримуємо список товарів
+        const productsResponse = await axios.get(PRODUCTS_API_URL);
+
+        // Фільтруємо товари за категорією
+        const filteredProducts = productsResponse.data.filter(
+          (product) => product.category === planetCategory._id
+        );
+
+        setProducts(filteredProducts);
+        console.log(products);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    }
+
+    // Викликаємо функцію для отримання даних
+    fetchData();
+  }, [planetName]);
+
   return (
     <div className="planet-info">
       <div className="planet-header">
@@ -18,6 +59,17 @@ function PlanetInfo({ planetName }) {
         </div>
       </div>
       <h1>Передогляд</h1>
+      <div className="planet-items-container">
+        {products.length > 0 ? (
+          products.map((item) => (
+            <div className="planet-item" key={item._id}>
+              {item.title}
+            </div>
+          ))
+        ) : (
+          <div>Завантаження товарів...</div>
+        )}
+      </div>
     </div>
   );
 }
